64ea8aad57eae97f8410656a0b899521b3dcdf2a
mscholz.dev <mscholz.dev@gmail.com>
2023-01-20 16:24:31 +0100

    add cookie

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
 import UserServiceClass from "./user.service.js";
 import UserValidatorClass from "../utils/validator/UserValidator.js";
-// import CookieClass from "../utils/Cookie.js";
+import CookieClass from "../utils/Cookie.js";
 
 // classes
 const UserService = new UserServiceClass();
 const UserValidator = new UserValidatorClass();
-// const Cookie = new CookieClass();
+const Cookie = new CookieClass();
 
 export default class UserController {
   async create(req: Request, res: Response) {
@@ -15,11 +15,11 @@ export default class UserController {
 
     const user = await UserService.create(schema);
 
-    // const jwt = Cookie.signJwt(user);
+    const jwt = Cookie.signJwt(user);
 
     res
       .status(200)
-      // .cookie("user", jwt, Cookie.cookieOptions())
+      .cookie("user", jwt, Cookie.cookieOptions())
       .end();
   }
 
@@ -31,11 +31,11 @@ export default class UserController {
       schema,
     );
 
-    // const jwt = Cookie.signJwt(user);
+    const jwt = Cookie.signJwt(user);
 
     res
       .status(200)
-      // .cookie("user", jwt, Cookie.cookieOptions())
+      .cookie("user", jwt, Cookie.cookieOptions())
       .end();
   }
 }

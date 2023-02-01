a57e5d74e5c019e9c79a499e3ae636732d814b33
mscholz.dev <mscholz.dev@gmail.com>
2023-02-01 11:08:03 +0100

    update cookie sameSite to none

--- src/utils/Cookie.ts
+++ src/utils/Cookie.ts
@@ -21,7 +21,7 @@ export default class Cookie {
           1000 * 60 * 60 * 24 * 7 * 8,
       ),
       path: "/",
-      sameSite: "lax",
+      sameSite: "none",
       secure: true,
       httpOnly: true,
     };

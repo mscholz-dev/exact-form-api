feab1d27860991ce9cce754f906f51807eb9f6af
mscholz.dev <mscholz.dev@gmail.com>
2023-02-01 11:48:50 +0100

    update cookie options

--- src/utils/Cookie.ts
+++ src/utils/Cookie.ts
@@ -10,7 +10,6 @@ export default class Cookie {
       // 8 weeks
       maxAge: 1000 * 60 * 60 * 24 * 7 * 8,
       path: "/",
-      sameSite: "lax",
       secure: true,
       httpOnly: true,
     };

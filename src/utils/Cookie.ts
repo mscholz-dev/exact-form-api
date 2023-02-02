ba4a1077651340b9ec320e5fcd7e4779727a22fd
mscholz.dev <mscholz.dev@gmail.com>
2023-02-02 11:08:10 +0100

    reset cookie settings

--- src/utils/Cookie.ts
+++ src/utils/Cookie.ts
@@ -10,10 +10,10 @@ export default class Cookie {
       // 8 weeks
       maxAge: 1000 * 60 * 60 * 24 * 7 * 8,
       path: "/",
-      // sameSite: "none",
+      sameSite: "lax",
       secure: true,
       httpOnly: true,
-      // domain: process.env.JWT_DOMAIN,
+      domain: process.env.JWT_DOMAIN,
     };
   }
 
